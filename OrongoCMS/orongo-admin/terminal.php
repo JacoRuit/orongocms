6c7a9017ddc66f833f9e42594e28b35068fd7ac7
ruitjaco <ruitjaco@gmail.com>
2012-01-11 15:16:03 +0000

    started with orongoterminal

---
+++ OrongoCMS/orongo-admin/terminal.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Using the great terminal jQuery plugin: http://terminal.jcubic.pl. Also credits for the guy(s) who made it, it rocks!
+ * @author Jaco Ruit
+ */
+
+require 'globals.php';
+
+
+
+
+$user = handleSessions();
+if($user == null)
+    header("Location: ../orongo-login.php");
+if($user->getRank() != RANK_ADMIN)
+    header("Location: index.php");
+
+$website_name = Settings::getWebsiteName();
+$website_url = Settings::getWebsiteURL();
+
+#   Template
+
+$smarty->assign("head_title", $website_name . " - Terminal - Logged in as " . $user->getName());
+$smarty->assign("website_url", $website_url);
+$smarty->assign("head", '<script src="' . $website_url .'js/jquery.mousewheel-min.js"></script><script src="' . $website_url .'js/jquery.terminal-0.4.6.min.js"></script><link href="' . $website_url .'orongo-admin/style/jquery.terminal.css" rel="stylesheet"/>');
+
+$smarty->display("terminal.orongo");
+
+?>
