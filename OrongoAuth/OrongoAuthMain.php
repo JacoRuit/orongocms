937539350e5bf8a50d4fa2d4e6681301585831d7
ruitjaco <ruitjaco@gmail.com>
2012-01-15 11:44:47 +0000

    nbproject included. first developments.

--- OrongoAuth/OrongoAuthMain.php
+++ OrongoAuth/OrongoAuthMain.php
@@ -1,7 +1,55 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+define("ORONGOAUTH_VERSION", 2);
+
+/**
+ * Main Class
+ * @author Jaco Ruit
  */
+
+class OrongoAuthMain implements IOrongoPlugin{
+    
+    private $settings;
+    
+    public function __construct(){
+        //Getting stored things as of rev 32
+        $this->settings = Plugin::getSettings();
+        
+        //Hook terminal plugin
+        if(!isset($this->settings["use_terminal"]))
+                $this->settings["use_terminal"] = true;
+        
+        if($this->settings["use_terminal"]){
+            if(!file_exists("OrongoAuthTerminal.php"))
+                throw new Exception("Terminal plugin missing!");
+            Plugin::hookTerminalPlugin(new OrongoAuthTerminal($this->settings));
+        }
+    }
+    
+     public function injectHTMLOnWebPage(){
+        return false;
+    }
+    
+    
+    public function getHTML(){
+        return null;
+    }
+    
+
+    public function getVersionNumber(){
+        return ORONGOAUTH_VERSION;
+    }
+    
+    public function onInstall(){
+        //TODO create database
+    }
+    
+    public function getSettings(){
+        //This does nothing
+        return null;
+    }
+
+}
+
+
 ?>
