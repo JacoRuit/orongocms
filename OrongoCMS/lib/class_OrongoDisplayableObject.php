4e790f4bbc609cfa28027eea6781b93fa49d064f
ruitjaco@gmail.com <ruitjaco@gmail.com@d6f3b955-8cd1-ed1c-c7b2-66fb8349f56a>
2012-01-17 23:31:30 +0000

    End of day revision, made some lil tweaks to Display, added abstract class OrongoDisplayableObject

---
+++ OrongoCMS/lib/class_OrongoDisplayableObject.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * OrongoDisplayableObject Class
+ *
+ * @author Jaco Ruit
+ */
+abstract class OrongoDisplayableObject implements IHTMLConvertable {
+    
+    abstract public function toHTML();
+    
+    final public function addToDisplay(){
+        getDisplay()->addHTML($this->toHTML());
+    }
+    
+    public function __toString(){
+        return "OrongoDisplayableObject";
+    }
+}
+
+?>
