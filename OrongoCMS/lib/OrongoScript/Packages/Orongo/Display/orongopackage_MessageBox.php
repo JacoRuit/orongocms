35dcb7087eefbf4a20b3b30037fb3d828e86f7bf
ruitjaco <ruitjaco@gmail.com>
2012-01-27 16:12:12 +0000

    tweaks

--- OrongoCMS/lib/OrongoScript/Packages/Orongo/Display/orongopackage_MessageBox.php
+++ OrongoCMS/lib/OrongoScript/Packages/Orongo/Display/orongopackage_MessageBox.php
@@ -11,10 +11,53 @@ class OrongoScriptMessageBox extends OrongoPackage {
         
     }
     public function getFunctions() {
-        require_once("functions/func_ShowMessageBox.php");
-        require_once("functions/func_MakeMessageBox.php");
         return array(new FuncShowMessageBox(), new FuncMakeMessageBox());
     }
 }
+
+# ---FUNCTIONS---
+
+/**
+ * show_msgbox OrongoScript function
+ *
+ * @author Jaco Ruit
+ */
+class FuncShowMessageBox extends OrongoFunction {
+    
+
+    public function __invoke($args) {
+        if(!isset($args[0]) || ($args[0] instanceof MessageBox) == false) throw new OrongoScriptParseException("Invalid arguments for show_msgbox()");
+        getDisplay()->addObject($args[0]);
+    }
+
+    public function getShortname() {
+        return "show_msgbox";
+    }
+}
+
+
+/**
+ * make_msgbox OrongoScript function
+ *
+ * @author Jaco Ruit
+ */
+class FuncMakeMessageBox extends OrongoFunction {
+    
+
+    public function __invoke($args) {
+        if(!isset($args[0]) || !is_string($args[0])) throw new OrongoScriptParseException("Invalid argument for make_msgbox()");
+        if(isset($args[1]) && is_string($args[1])) $title = $args[1];
+        else $title = "OrongoCMS";
+       
+        return new OrongoVariable(new MessageBox($args[0], $title));
+    }
+
+    public function getShortname() {
+        return "make_msgbox";
+    }
+}
+
+
+# ---END FUNCTIONS---
 
 ?>
---
+++ OrongoCMS/lib/OrongoScript/Packages/Orongo/Plugin/orongopackage_Settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Settings OrongoScript Package
+ *
+ * @author Jaco Ruit
+ */
+class OrongoScriptSettings {
+    //put your code here
+}
+
+?>
