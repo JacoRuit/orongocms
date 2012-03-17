c8e2898fa7e9402dc4a00d23ac360c84db752df3
ruitjaco@gmail.com <ruitjaco@gmail.com@d6f3b955-8cd1-ed1c-c7b2-66fb8349f56a>
2012-03-17 11:28:25 +0000

    index.php contained a little test, deleted it

--- OrongoCMS/index.php
+++ OrongoCMS/index.php
@@ -7,31 +7,6 @@ require 'startOrongo.php';
 startOrongo();
 
 
-class Test{
-    private $array;
-    public $a;
-    
-    public function __construct(){
-        $this->array = array();
-        $a =  debug_backtrace();
-        var_dump($a[1]['class']);
-        var_dump($a);
-    }
-    
-    public function __invoke(){
-        foreach($this->array as $func){ $func(); }
-    }
-    
-    public function add($method){
-        var_dump($method instanceof Closure);
-        $this->array[count($this->array)] = $method;
-        var_dump(func_get_args($method));
-    }
-    
-}
-
-
-
 setCurrentPage('index');
 $index = new IndexFrontend();
 $index->main(array('time' => time()));
